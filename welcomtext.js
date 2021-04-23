e8099173867288a799c8f9dd0dfa8ab59bf71607
arima <76896919+whitereaper25@users.noreply.github.com>
2021-04-23 21:13:10 +0530

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,10 +1,10 @@
 const welcome = (number, groupname) => {
-    return `𝐇𝐨𝐥𝐚 @${number}. 𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨 𝐚 ${groupname}`
+    return `𝐇𝐨𝐥𝐚 @${number}. Welcome to ${groupname}`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `𝐐𝐮𝐞 𝐛𝐮𝐞𝐧𝐨 𝐪𝐮𝐞 𝐭𝐞 𝐟𝐮𝐢𝐬𝐭𝐞 @${number}.𝐧𝐨 𝐭𝐞 𝐪𝐮𝐞𝐫𝐢́𝐚𝐦𝐨𝐬.
-𝐄𝐬𝐩𝐞𝐫𝐨 𝐧𝐮𝐧𝐜𝐚 𝐯𝐮𝐞𝐥𝐯𝐚𝐬 👋`
+    return `OH you left @${number}.I wont miss you.
+anyway see ya👋`
 }
 exports.bye = bye
